d7abb51e0f6989100e72023b1ceed01a48d698d5
Lazaro <lazaro.fonseca@ebix.com>
2020-03-03 12:12:53 -0300

    init

---
+++ app.js
@@ -0,0 +1,16 @@
+const logger = require('./logger');
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+	logger.info('hello');
+	res.status(200).json({ message: 'hello' });
+});
+
+app.listen(3000, () => {
+	console.log('running in port 3000');
+});
